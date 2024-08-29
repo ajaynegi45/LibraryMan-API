a6002f639ec7da0c025baae833c10c1aa2389ae3
Ajay Negi <ajaynegi3345@icloud.com>
2024-08-29 23:15:05 +0530

    Add notification service

--- src/main/java/com/libraryman_api/repository/MemberRepository.java
+++ src/main/java/com/libraryman_api/repository/MemberRepository.java
@@ -1,11 +1,18 @@
 package com.libraryman_api.repository;
 
+import com.libraryman_api.entity.Borrowings;
 import com.libraryman_api.entity.Members;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Members, Integer> {
+
+    Optional<Members> findByMemberId(int memberId);
+
 
 /**
  * SELECT SUM(amount) AS totalFines
