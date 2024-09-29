f6e1213714c052fb463542b2b3de3c806f81e3cd
Ajay Negi <ajaynegi3345@icloud.com>
2024-09-29 11:15:52 +0530

    Add security

--- src/main/java/com/libraryman_api/Application.java
+++ src/main/java/com/libraryman_api/Application.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-public class LibrarymanApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibrarymanApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ src/main/java/com/libraryman_api/security/SignupController.java
@@ -0,0 +1,4 @@
+package com.libraryman_api.security;
+
+public class SignupController {
+}
